e1dbc41d1106cfd80d56fab2af015d4d1d80ab9e
Ayaz Ayupov <ayaz@releaseband.com>
2021-11-26 11:15:12 +0300

    GRP-890 #time 2m changed interfaces

--- adapter/adapter.go
+++ adapter/adapter.go
@@ -1,20 +1,10 @@
 package adapter
 
-type SeedGenerator interface {
-	Seed() int64
-}
-
 type RNG interface {
 	Random(min, max uint32) uint32
-	YesNo() bool
+}
+
+type Random interface {
+	RNG
 	SetSeed(seed int64)
-}
-
-
-type POOL interface {
-	Get() (RNG, int64, error)
-}
-
-type PoolMaker interface {
-	Make(generator SeedGenerator) POOL
 }
